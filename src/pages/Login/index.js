25941274f386fbeec64e9a95e28e6688b7c526ad
zhq123 <2847492481@qq.com>
2024-04-06 03:11:03 +0800

    登录表单校验

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,41 @@
-const Login = () => {
-  return <div>this is login</div>;
-};
+import "./index.scss"
+import { Card, Form, Input, Button } from "antd"
+import logo from "@/assets/logo.png"
 
-export default Login;
+const Login = () => {
+  const onFinish = (values) => {
+    console.log(values)
+  }
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form onFinish={onFinish} validateTrigger="onBlur">
+          <Form.Item
+            name="mobile"
+            rules={[
+              { required: true, message: "请输入手机号" },
+              { pattern: /^1[3-9]\d{9}$/, message: "手机号格式错误" }
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[{ required: true, message: "请输入验证码" }]}
+          >
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
+}
+
+export default Login
