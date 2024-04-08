e2e15b93b11b663d9db171e43e07e8de9b8119c6
zhq123 <2847492481@qq.com>
2024-04-08 20:44:22 +0800

    layout页面搭建

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { RouterProvider } from "react-router-dom"
 import router from "./router"
 import store from "./store"
 import { Provider } from "react-redux"
+import "normalize.css"
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
